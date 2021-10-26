57d15486d8e9fde8b173d15609058679af623bef
Charles Coffie <coffie2000us@gmail.com>
2021-10-26 16:49:37 -0400

    initial commit

---
+++ VariablesExercise2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VariablesExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myCat = "Peace";
+            int catAge = 5;
+            char firstLetter = 'P';
+            bool isBlack = true;
+            double hisWeight = 8;
+            decimal height = 9.1m;
+
+            Console.WriteLine($"The name of my dog is {myCat}. He is about {catAge} years old " +
+                $"and his first initial is {firstLetter}. It is {isBlack} that {myCat} is black. He is " +
+                $"{hisWeight} lbs and has a height of {height} inches");
+        }
+    }
+}
